eb0fc5a830df350275a3a3cfc9336518a0d30cbf
fkokro <fkokro@gmail.com>
2020-03-29 16:01:34 -0400

    Added Data

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,4 +1,17 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+//Print Data in console
+//console.log(tableData);
+
+//Use D3 to call table body from HTML
+var tbody = d3.select("tbody");
+
+//Create function to populate table with data from data.js file 
+data.forEach((sightings) => {
+    var row = tbody.append("tr");
+    Object.entries(sightings).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
